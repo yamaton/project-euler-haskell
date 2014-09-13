4a59d91b232060142f68acc8df3dd56c4fc39abc
yamato matsuoka <yamaton@gmail.com>
2014-09-12 23:21:51 -0600

    Minor fixes

--- Solutions41-50.hs
+++ Solutions41-50.hs
@@ -68,26 +68,29 @@ wordScore = sum . map (\c -> Char.ord c - base)
 -- [Sub-string divisibility](http://projecteuler.net/problem=43)
 
 prob043 :: Int
-prob043 = sum . filter isPandigital $ map Utils.fromDigits out
+prob043 = sum . map Utils.fromDigits . filter isPandigital $ out
   where
     (xs:xss) = reverse . map threeDigitMultiples $ [2,3,5,7,11,13,17]
     out = map prependComplement $ connectList xss xs
 
 
--- Pad 0 to the left such that the list has n elements
-padLeft :: Int -> [Int] -> [Int]
-padLeft n xs
+-- | Pad 0 to the left such that the list has n elements
+-- >>> padLeft 0 5 [1,2,3]
+-- [0,0,1,2,3]
+-- >>> padLeft '.' 8 "abc"
+-- ".....abc"
+padLeft :: a -> Int -> [a] -> [a]
+padLeft e n xs
   | len == n  = xs
-  | otherwise = (replicate (n - len) 0) ++ xs
+  | otherwise = (replicate (n - len) e) ++ xs
   where len = length xs
 
 -- return three-digit multiples of n, in digits form
 threeDigitMultiples :: Int -> [[Int]]
-threeDigitMultiples n = map (padLeft 3 . Utils.integerDigits) [n, 2*n..999]
+threeDigitMultiples n = map (padLeft 0 3 . Utils.integerDigits) [n, 2*n..999]
 
 
-{- | [FIXME] This is terrible and a source of BUG!
-
+{- |
 >>> connectList [["bcd", "dfa"], ["zdd", "abc"]] ["zds", "dar", "add"]
 []
 
@@ -103,18 +106,19 @@ connectList (candids:rest) paths = connectList rest newPaths
     newPaths = [ (z:p) | (z:zs) <- candids, p <- paths, zs == take 2 p ]
 
 
--- Prepend one of the complement digit to the integer list xs
+-- | Prepend the largest complement digit to an int digit list 
+-- >>> prependComplement [1,3,2,4,6,5,7,7,0]
+-- [9,1,3,2,4,6,5,7,7,0]
+
 prependComplement :: [Int] -> [Int]
 prependComplement xs = p:xs
-  where
-    (p:_) = [9,8..1] \\ xs
+  where (p:_) = [9,8..1] \\ xs
 
 
-isPandigital :: Int -> Bool
-isPandigital n = length xs == 10 && oneToNine == IntSet.fromList xs
-  where
-    oneToNine = IntSet.fromList [0..9]
-    xs = Utils.integerDigits n
+isPandigital :: [Int] -> Bool
+isPandigital xs = (length xs == 10) && (oneToNine == IntSet.fromList xs)
+  where oneToNine = IntSet.fromList [0..9]
+
 
 
 -- | Problem 44
@@ -131,42 +135,35 @@ prob044 = head [ pm |  m <- [1..2000],
 pentagonal :: Int -> Int
 pentagonal n = n * (3*n -1) `div` 2
 
-
 -- | 
 -- >>> isPentagonal 5482660
 -- True
 isPentagonal :: Int -> Bool
-isPentagonal n = n == d
+isPentagonal n = (n == d)
   where 
     estimate = floor $ (1 + sqrt (1 + 24 * fromIntegral n)) / 6
     xs = drop (estimate - 1) $ map pentagonal [1..]
     d  = head $ dropWhile (< n) xs
-    
+
 
 
 -- | Problem 45
 -- [Triangular, pentagonal, and hexagonal](http://projecteuler.net/problem=45)
 -- Find the lowest triangular number x > 40755 that is also pentagonal and hexagonal.
 prob045 :: Int
-prob045 = last $ IntSet.toList $ foldr1 IntSet.intersection [ts,ps,hs]
+prob045 = head $ IntSet.toList $ foldr1 IntSet.intersection [ts,ps,hs]
   where 
     nMax = 100000    -- [FIXME] This choice of parameter requires try and error!
-    ts = IntSet.fromList $ map triangleNumber [1..nMax]
-    ps = IntSet.fromList $ map pentagonalNumber [1..nMax]
-    hs = IntSet.fromList $ map hexagonalNumber [1..nMax]
-
-triangleNumber   :: Int -> Int
-triangleNumber n   = n * (n+1) `div` 2
-pentagonalNumber :: Int -> Int
-pentagonalNumber n = n * (3*n - 1) `div` 2
-hexagonalNumber  :: Int -> Int
-hexagonalNumber n  = n * (2*n - 1)
+    ts = toIntSet $ scanl1 (+) [1..]
+    ps = toIntSet $ scanl1 (+) [1,4..]
+    hs = toIntSet $ scanl1 (+) [1,5..]
+    toIntSet = IntSet.fromList . takeWhile (< nMax) . dropWhile (<= 40755)
 
 
 
 -- | Problem 46
 -- [Goldbach's other conjecture](http://projecteuler.net/problem=46)
-
+-- Find the smallest odd composite that cannot be written as the sum of a prime and twice a square.
 prob046 :: Int
 prob046 = head $ dropWhile isGoldBach oddComposites
   where
@@ -175,7 +172,7 @@ prob046 = head $ dropWhile isGoldBach oddComposites
 isGoldBach :: Int -> Bool
 isGoldBach n = any isSquare $ map (\p -> ((n - p) `div` 2)) ps
   where 
-    -- ps: primes sequence from 3 to (n-2)
+    -- ps is primes sequence from 3 to (n-2)
     ps = drop 1 $ Utils.primesTo (n-2)  
 
 isSquare :: Int -> Bool
@@ -193,11 +190,17 @@ prob047 :: Int
 prob047 = conseqNDPS 4
 
 conseqNDPS :: Int -> Int
-conseqNDPS n = head . head $ dropWhile (\xs -> replicate n n /= map ndps xs) $ Utils.partition n 1 [1..]
+conseqNDPS n = head . head . dropWhile (\xs -> replicate n n /= map ndps xs) 
+                    $ Utils.partition n 1 [1..]
 
--- ndps (Number of Distinct Prime Factors)
+-- | ndps (Number of Distinct Prime Factors)
+-- >>> ndps 12
+-- 2
+-- >>> ndps 210
+-- 4
 ndps :: Int -> Int
 ndps = length . Utils.factorInteger
+
 
 
 -- | Problem 48
@@ -206,10 +209,10 @@ ndps = length . Utils.factorInteger
 -- Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 prob048 :: Int
-prob048 = fromIntegral $ n `mod` (10^10)
+prob048 = fromIntegral . (`mod` (10^10)) . sum . map (\i -> i^i) $ xs
   where
-    n :: Integer
-    n = sum . map (\i -> i^i) $ [1..1000]
+    xs = [1..1000] :: [Integer]
+
 
 
 -- | Problem 49
@@ -219,12 +222,12 @@ prob049 :: Int
 prob049 = Utils.fromDigits . concatMap Utils.integerDigits $ last troikas
     where
       fourDigitPrimes = filter (> 1000) $ Utils.primesTo 9999
-      troikas = [ troika | 
-                    x   <- fourDigitPrimes,
-                    inc <- [2,4..(5000 - x `div` 2)],
-                    let troika = [x, x+inc, x+2*inc],
-                    all Utils.isPrime troika,
-                    allTheSame $ map (List.sort . Utils.integerDigits) troika ]
+      troikas = [ troika | x   <- fourDigitPrimes
+                ,          inc <- [2,4..(5000 - x `div` 2)]
+                ,          let troika = [x, x+inc, x+2*inc]
+                ,          all Utils.isPrime troika
+                ,          allTheSame $ map (List.sort . Utils.integerDigits) troika 
+                ]
 
 allTheSame :: Eq a => [a] -> Bool
 allTheSame xs = and $ zipWith (==) xs (tail xs)
@@ -237,18 +240,19 @@ allTheSame xs = and $ zipWith (==) xs (tail xs)
 -- contains 21 terms, and is equal to 953. Which prime, below one-million, 
 -- can be written as the sum of the most consecutive primes?
 prob050 :: Int
-prob050 = head [s | len <- [maxLen, maxLen-1 .. 2],
-                    i   <- [0..(maxLen - len)],
-                    let s = sum $ take len . drop i $ xs,
-                    Utils.isPrime s ]
+prob050 = head [ s | len <- [maxLen, maxLen-1 .. 2]
+               ,     i   <- [0..(maxLen - len)]
+               ,     let s = sum $ take len . drop i $ xs
+               ,     Utils.isPrime s 
+               ]
   where
     -- The choice of 10000 is little arbitrary ... just a large number
     maxLen = length . takeWhile (< 1000000) . scanl1 (+) $ Utils.primes 10000
     xs = Utils.primes maxLen
 
 
--- Interface
 
+-- Interface
 select :: Int -> IO Int
 select 42 = prob042
 select n = return $ [prob041,       0, prob043, prob044, prob045,
@@ -256,7 +260,4 @@ select n = return $ [prob041,       0, prob043, prob044, prob045,
 
 main :: IO ()
 main = getArgs >>= return . read . head >>= select >>= print
-
-
-
 
